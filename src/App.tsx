122d8cdece245500168c8c67f5390ae4722e0fb8
Masud Afsar <masud@afsar.info>
2022-05-22 10:17:12 +0430

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, Text} from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <Text>Malltina App</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
