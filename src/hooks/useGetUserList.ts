14e26ab0fe9e6fd8c36a3f7bc52d93cbe2643672
Masud Afsar <masud@afsar.info>
2022-05-24 12:05:54 +0430

    Implement api response type

--- src/hooks/useGetUserList.ts
+++ src/hooks/useGetUserList.ts
@@ -2,6 +2,6 @@ import {useGetRequest} from 'hooks/useGetRequest';
 import {UsersParams, UsersResponse} from 'types/api/users';
 import {DependencyList} from 'react';
 
-export const useGetUserList = (params: UsersParams, deps: DependencyList) => {
+export const useGetUserList = (params: UsersParams, deps?: DependencyList) => {
   return useGetRequest<UsersResponse, UsersParams>('/', params, deps);
 };
