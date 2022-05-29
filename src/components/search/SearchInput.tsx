baad60a77dc205033d158902e13c4a017592a39f
emel95 <amalmosavi95@gmail.com>
2022-05-29 21:35:19 +0430

    add query search in context

--- src/components/search/SearchInput.tsx
+++ src/components/search/SearchInput.tsx
@@ -1,47 +1,50 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import styles from './SearchInputStyles';
-import { TextInput, View ,Dimensions} from 'react-native';
+import { TextInput, View, Dimensions } from 'react-native';
 import MaterialIcon from 'react-native-vector-icons/FontAwesome';
 import { useSearchContext } from 'context/SearchContextProvider';
 import { useUsersContext } from 'context/UsersContextProvider';
-const { height} = Dimensions.get('screen');
+const { height } = Dimensions.get('screen');
 
 interface Props { }
 
 export const SearchInput: React.FC<Props> = () => {
-  const [searchName, setSearchName] = useState<string>('');
+  // const [searchName, setSearchName] = useState<string>('');
   const [getUser, setUsers] = useSearchContext();
   const [{ users: cacheUsers }] = useUsersContext();
+
 
   useEffect(() => {
     onPressSearch();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [searchName]);
+    // }, [searchName]);
+  }, [getUser.query]);
 
   const onChangeText = (text: string) => {
-    setSearchName(text);
-    debounceSearch()
+    // setSearchName(text);
+    setUsers({ query: text })
+    // debounceSearch()
   };
 
-  const debounce = (func: () => {}, delay: number) => {
-    let timeOutId:ReturnType<typeof setTimeout>;
-    return () => { 
-      if(timeOutId) clearTimeout(timeOutId);
-      timeOutId=setTimeout(()=>{
-        // func.apply(null,args);
-        func();
-      },delay);
-    }
-  }
+  // const debounce = (func: () => {}, delay: number) => {
+  //   let timeOutId: ReturnType<typeof setTimeout>;
+  //   return () => {
+  //     if (timeOutId) clearTimeout(timeOutId);
+  //     timeOutId = setTimeout(() => {
+  //       // func.apply(null,args);
+  //       func();
+  //     }, delay);
+  //   }
+  // }
+
 
   const onPressSearch = useCallback(() => {
-    console.log('searchName', searchName);
-    if (searchName) {
+    console.log('searchName', getUser.query);
+    if (getUser.query) {
       const newData = cacheUsers?.filter(item => {
         const itemData = (item.name.first + item.name.last).toLowerCase();
-        const textData = searchName.replace(/\s/g, '').toLowerCase();
-
-        return itemData.indexOf(textData) > -1;
+        const textData = getUser?.query?.replace(/\s/g, '').toLowerCase();
+        if (textData) return itemData.indexOf(textData) > -1;
       });
       console.log('newData', newData);
       if (newData) {
@@ -56,16 +59,16 @@ export const SearchInput: React.FC<Props> = () => {
 
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [searchName]);
+  }, [getUser.query]);
 
-   const debounceSearch=debounce(()=>onPressSearch,500)
+  // const debounceSearch = debounce(() => onPressSearch, 500)
 
 
   return (
     <View style={styles.cardSearch}>
       <MaterialIcon
         name="search"
-        size={height/23}
+        size={height / 23}
         color="#fbbf24"
         onPress={onPressSearch}
       />
@@ -73,7 +76,7 @@ export const SearchInput: React.FC<Props> = () => {
         style={styles.inputText}
         placeholder="Search Name"
         underlineColorAndroid="transparent"
-        value={searchName}
+        value={getUser.query}
         onChangeText={onChangeText}
         onSubmitEditing={onPressSearch}
       />
